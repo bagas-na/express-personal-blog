b5248273808e6106fdb3ef877ef38283a0bf8ad9
bagas-na <bagasnurseno@gmail.com>
2024-08-28 23:22:09 +0700

    add md parser to html and html sanitizer to render

---
+++ public/articles/index.md
@@ -0,0 +1,13 @@
+Get the **inside scoop** on my latest **adventures** and *misadventures*—you **won’t believe** what happened this week! From:
+
+- *Cringe-worthy* awkward encounters
+- **Shocking** surprises
+- **Jaw-dropping** revelations
+
+…it’s all here, and you’re going to want to hear *every* juicy detail.
+
+Join me for:
+
+- The **behind-the-scenes** drama
+- The *secrets* everyone’s talking about
+- The stories you **can’t afford to miss**!
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,28 @@
-import express from 'express';
+import express from "express";
+import marked from "marked";
+import fs from "node:fs/promises";
+import path from "path";
+import sanitize from "sanitize-html";
 
-let router = express.Router();
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const ARTICLE_FOLDER = "../../public/articles/";
+const ARTICLE_FILE = "index.md";
+const title = "Welcome to My Personal Blog";
+const subtitle = "Unbelievable shenanigans awaits!";
+
+router.get("/", async function (req, res, next) {
+  const mdFilePath = path.join(__dirname, ARTICLE_FOLDER, ARTICLE_FILE);
+
+  try {
+    const data = await fs.readFile(mdFilePath, "utf8");
+    const rawHtml = await marked.parse(data);
+    const html = sanitize(rawHtml)
+    res.render("index", { title, subtitle, text: html });
+  } catch (e) {
+    res.status(500).send(`Error reading markdown from ${mdFilePath}`);
+  }
 });
 
-module.exports = router;
-
-export default router
+export default router;
